aad1814e452ba24fbd2de855d3a32773188f69db
Luis Sánchez-Arévalo Rionegro <luis.sanchezarevalo.developer@gmail.com>
2020-04-28 19:19:47 +0200

    añadida funcion de retorno de datos al html, retocada funcion getExcuse y getRandomNumber

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-var who = ['the dog','my granma','his turtle','my bird'];
+var who = ['The dog','My granma','His turtle','My bird'];
 var action = ['eat','pissed','crushed','broked'];
 var what = ['homework','keys','fruit','food'];
 var when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
 
+window.onload = () => {
+    document.getElementById('excuse').innerHTML = getExcuse();
+}
+
 function getExcuse() {
-    let position = getRandomNumber();
-    let excuse = ''.concat(who[position],' ',action[position],' ',what[position],' ',when[position]);
+    let listExcuses = [who, action, what, when];
+    let excuse = '';
+
+    for (const partExcuse of listExcuses) {
+        let position = getRandomNumber(partExcuse);
+        excuse = excuse.concat(partExcuse[position],' ');
+    }
+    
     return excuse;    
 }
-console.log(getExcuse());
 
-function getRandomNumber() {
-    return Math.floor(Math.random() * 4); 
+function getRandomNumber(partExcuse) {
+    return Math.floor(Math.random() * partExcuse.length); 
 }
